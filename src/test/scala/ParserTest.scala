23e1fd3a7e699ab60bb9f06c10affcf43c19331f
Matthew Dulworth <mhdulworth@gmail.com>
2020-12-19 13:09:45 -0600

    Added testing methods

--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -7,21 +7,41 @@ class ParserTest extends FunSuite {
     case (None, _) => false
     case (None, None) => true
     case (e, a) => e.get.deepEquals(a.get)
+    case (_, _) => false
   }
+
+  def printAST(t: Option[ASTree]): Unit = t match {
+    case None => println("Parse terminated with None")
+    case _ => TreePrinter.print(t.get)
+  }
+
+  def testAST(input: String, expected: Option[ASTree], showInfo: Boolean): Unit = {
+    val actual = Parser.parse(input)
+    val testCondition = equalASTs(expected, actual)
+
+    if (showInfo) {
+      println("actual: ")
+      printAST(actual)
+
+      if (!testCondition) {
+        println("expected: ")
+        printAST(expected)
+      }
+    }
+
+    assert(testCondition)
+  }
+
+  val showInfo = true
 
   test("Parser should parse simple binary operations") {
 
-    val tree = Node(EmptyNode, ADD)
-    tree.left = Node(tree, NumberToken(2))
-    tree.right = Node(tree, NumberToken(3))
-    val expected = Option(tree)
-
-    val actual = Parser.parse("2 + 3")
-    assert(equalASTs(expected, actual))
-
-    actual match {
-      case None => println("no tree")
-      case _ => TreePrinter.print(actual.get)
-    }
+    val expected = Option({
+      val tree = Node(EmptyNode, ADD)
+      tree.left = Node(tree, NumberToken(2))
+      tree.right = Node(tree, NumberToken(4))
+      tree
+    })
+    testAST("2 + 3", expected, showInfo)
   }
 }
