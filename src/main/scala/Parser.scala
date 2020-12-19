66ac891181713bd778a174272918962e5b9aeda6
Matthew Dulworth <mhdulworth@gmail.com>
2020-12-19 11:26:30 -0600

    made methods that didnt need to be public private

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -8,8 +8,7 @@ case object Parser {
     buildTree(lexer, currentNode)
   }
 
-  @tailrec
-  private def buildTree(lexer: Lexer, node: Tree): Option[Tree] = {
+  @tailrec private def buildTree(lexer: Lexer, node: Tree): Option[Tree] = {
     val token = lexer.getNextToken
 
     token match {
@@ -22,7 +21,7 @@ case object Parser {
     }
   }
 
-  def traverseCurrentUp(startNode: Tree, token: Token): Tree = token match {
+  private def traverseCurrentUp(startNode: Tree, token: Token): Tree = token match {
     case OPEN_PAREN | NEGATION => startNode
     case _ =>
       var currentNode = startNode
@@ -32,17 +31,17 @@ case object Parser {
       currentNode
   }
 
-  def addNode(node: Tree, token: Token): Tree = token match {
+  private def addNode(node: Tree, token: Token): Tree = token match {
     case CLOSE_PAREN => closeParens(node)
     case _ => insertNewNode(node, token)
   }
 
-  def closeParens(node: Tree): Tree = node.parent match {
+  private def closeParens(node: Tree): Tree = node.parent match {
     case parent: Node => parent.setRight(node.right)
     case EmptyNode => node.right.asInstanceOf[Node].resetParent()
   }
 
-  def insertNewNode(currentNode: Tree, token: Token): Tree = token match {
+  private def insertNewNode(currentNode: Tree, token: Token): Tree = token match {
     case token if token.isLessThan(currentNode.token) =>
       assert(currentNode.parent == EmptyNode)
       val newNode = Node(EmptyNode, token)
@@ -52,7 +51,7 @@ case object Parser {
       newNode.setLeft(currentNode.right)
   }
 
-  def treeRoot(startNode: Tree): Option[Tree] = {
+  private def treeRoot(startNode: Tree): Option[Tree] = {
     var node = startNode
     while (node.parent != EmptyNode) {
       node = node.parent
