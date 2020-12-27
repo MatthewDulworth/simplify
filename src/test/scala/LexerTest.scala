dded0ac3182888f45b9f4f6e337378887bac088d
Matthew Dulworth <mhdulworth@gmail.com>
2020-12-27 10:54:50 -0600

    Added lexer tests

--- src/test/scala/LexerTest.scala
+++ src/test/scala/LexerTest.scala
@@ -3,13 +3,33 @@ import org.scalatest.FunSuite
 class LexerTest extends FunSuite {
 
   test("empty input") {
-    val exp = List(END)
-    testLexer("", exp)
+    testLexer("", Nil)
   }
 
-  test("numbers")(pending)
+  test("decimal numbers") {
+    testLexer("3.04420", Decimal(3.04420) :: Nil)
+    testLexer(".0", Decimal(0) :: Nil)
+  }
 
-  test("invalid numbers")(pending)
+  test("variables") {
+    testLexer("xasd", Variable("xasd") :: Nil)
+  }
+
+  test("named constants") {
+    testLexer("pi", PI :: Nil)
+    testLexer("e", E :: Nil)
+  }
+
+  test("invalid numbers") {
+    testLexer("3.2424.5", InvalidNumber("3.2424.5") :: Nil)
+    testLexer(".", InvalidNumber(".") :: Nil)
+  }
+
+  test("all numbers") {
+    testLexer("3.13pi0.0e2.1111x9uy",
+      Decimal(3.13) :: PI :: Decimal(0) :: E :: Decimal(2.1111) :: Variable("x") :: Decimal(9) :: Variable("uy") :: Nil
+    )
+  }
 
   test("binary operators")(pending)
 
@@ -27,7 +47,8 @@ class LexerTest extends FunSuite {
 
   def testLexer(in: String, exp: List[Token]): Unit = {
     val tokens = getTokens(new Lexer(in)).reverse
-    assert(tokens == exp)
+    val expected = END :: exp.reverse
+    assert(tokens == expected.reverse)
   }
 
   def getTokens(lexer: Lexer, list: List[Token] = Nil): List[Token] = {
