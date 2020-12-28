a07c05ee1b9b8c3a05ec195d21e6e32345da6b03
Matthew Dulworth <mhdulworth@gmail.com>
2020-12-27 21:55:55 -0600

    scaladoc for ASTree

--- src/main/scala/ASTree.scala
+++ src/main/scala/ASTree.scala
@@ -1,15 +1,29 @@
+
+/**
+ * Abstract Syntax Tree. Represents an expression.
+ */
 sealed trait ASTree {
   val left: ASTree
   val right: ASTree
   val token: Token
 }
 
+/**
+ * Represents an empty expression. Used to terminate branches of a tree.
+ */
 case object Empty extends ASTree {
   override val left: ASTree = Empty
   override val right: ASTree = Empty
   override val token: Token = END
 }
 
+/**
+ * Represents a non-empty expression.
+ *
+ * @param token The Number/Operation that the node is.
+ * @param left  The left child of the node. Empty if a Number.
+ * @param right The right child of the node. Empty if a Number or a UnaryOperator
+ */
 case class Node(token: Token, left: ASTree = Empty, right: ASTree = Empty) extends ASTree {
   override def toString: String = token match {
     case n: Number => n.symbol
