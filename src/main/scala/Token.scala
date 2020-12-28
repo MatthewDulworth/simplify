062615c180830dd172929041968c4df8c0216657
Matthew Dulworth <mhdulworth@gmail.com>
2020-12-28 10:56:04 -0600

    Changed rule formatting for clairty

--- src/main/scala/Token.scala
+++ src/main/scala/Token.scala
@@ -152,9 +152,9 @@ case object ADD extends LeftAssoc {
   // 0 + x => x
   // x + x => 2x
   override def operation(left: ASTree, right: ASTree): ASTree = (left.token, right.token) match {
-    case (Decimal(0), expr) => Node(expr)
-    case (expr, Decimal(0)) => Node(expr)
-    case (a: Decimal, b: Decimal) => Node(Decimal(a.value + b.value))
+    case (Decimal(0), x) => Node(x)
+    case (x, Decimal(0)) => Node(x)
+    case (Decimal(a), Decimal(b)) => Node(Decimal(a + b))
     case (x, y) if x == y => Node(MULTIPLY, Node(Decimal(2)), Node(x))
     case (_, _) => Node(ADD, left, right)
   }
@@ -168,11 +168,10 @@ case object SUBTRACT extends LeftAssoc {
   // 0 - x => neg(x)
   // x - x => 0
   override def operation(left: ASTree, right: ASTree): ASTree = (left.token, right.token) match {
-    case (expr, Decimal(0)) => Node(expr)
-    case (Decimal(0), expr) => Node(NEGATE, Node(expr))
-    case (l, r) if l == r => Node(Decimal(0))
-    case (a: Decimal, b: Decimal) => val d = a.value - b.value
-      if (d < 0) Node(NEGATE, Node(Decimal(Math.abs(d)))) else Node(Decimal(d))
+    case (x, Decimal(0)) => Node(x)
+    case (Decimal(0), x) => Node(NEGATE, Node(x))
+    case (x, y) if x == y => Node(Decimal(0))
+    case (Decimal(a), Decimal(b)) => if (b > a) Node(NEGATE, Node(Decimal(Math.abs(a - b)))) else Node(Decimal(a - b))
     case (_, _) => Node(SUBTRACT, left, right)
   }
 }
@@ -189,10 +188,10 @@ case object MULTIPLY extends LeftAssoc {
   override def operation(left: ASTree, right: ASTree): ASTree = (left.token, right.token) match {
     case (_, Decimal(0)) => Node(Decimal(0))
     case (Decimal(0), _) => Node(Decimal(0))
-    case (expr, Decimal(1)) => Node(expr)
-    case (Decimal(1), expr) => Node(expr)
-    case (l, r) if l == r => Node(POWER, left, Node(Decimal(2)))
-    case (a: Decimal, b: Decimal) => Node(Decimal(a.value * b.value))
+    case (x, Decimal(1)) => Node(x)
+    case (Decimal(1), x) => Node(x)
+    case (Decimal(a), Decimal(b)) => Node(Decimal(a * b))
+    case (x, y) if x == y => Node(POWER, Node(x), Node(Decimal(2)))
     case (_, _) => Node(MULTIPLY, left, right)
   }
 }
